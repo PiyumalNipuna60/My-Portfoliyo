b74b527bb2bc32f6f556bbd0da32ba177208bfec
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-10-10 13:33:15 +0530

    Add display load icon

---
+++ mainScript.js
@@ -0,0 +1,11 @@
+
+// ===============load display====================
+$(window).on('load', function () {
+    console.log("Window on load");
+
+    // $("#loader").css('display','none');
+    $("#loader").fadeOut(1000);
+});
+
+
+// ===============Assigment====================
